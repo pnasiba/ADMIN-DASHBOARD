35ca3a31df0ecf3517a58eb865c90d58d3ef27b3
pnasiba <pnasiba23.06@gmail.com>
2024-03-04 18:23:15 +0500

    task done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,15 @@
 let dropdown = $(".dropdown"),
   dropdownMenu = $(".dropdown-menu"),
-  navMenu = $(".nav-menu")
-
-
+  navMenu = $(".nav-menu"),
+  asideMenu = $("#aside"),
+  toggleMenu = $(".toggle-menu")
 
 // Dropdown menu funtionally
-dropdown.addEventListener("click", function(e){
-    e.preventDefault()
-    dropdownMenu.classList.toggle("show-dropdown")
-})
+dropdown.addEventListener("click", function (e) {
+  dropdownMenu.classList.toggle("show-dropdown");
+});
 
 // window.addEventListener("click", function(e){
-    
 
 //     if(e.target.nodeName!="HTML"){
 
@@ -29,17 +27,42 @@ dropdown.addEventListener("click", function(e){
 // Navigation menu funtionally
 
 
-  function renderNavigationMenu(data){
-    if(data.length){
-        data.forEach(el => {
-            const listItem = createElement("li",classListItem, 
-            `<a href="${el.link} class="gap-x-[12px] text-xl flex items-center">
+
+function renderNavigationMenu(data) {
+  if (data.length) {
+    data.forEach((el) => {
+      const listItem = createElement(
+        "li",
+        classListItem,
+        `<a href="${el.link} class="gap-x-[12px] text-xl flex items-center">
              <i class="bi ${el.icon} text-2xl"></i>
-             <span>${el.title}</span> </a>`);
+             <span>${el.title}</span> </a>`
+      );
 
-            navMenu.appendChild(listItem)
-        });
-    }
+      navMenu.appendChild(listItem);
+    });
   }
+}
 
-  renderNavigationMenu(navigationMenu)
+renderNavigationMenu(navigationMenu);
+
+toggleMenu.addEventListener("click", () => {
+  asideMenu.classList.toggle("hide-aside");
+});
+
+
+
+(function(){
+let token = localStorage.getItem("token")
+
+
+if(!token){
+    window.location.href = "../../pages/login.html"
+}
+}())
+
+
+function logOut(){
+    localStorage.removeItem("token")
+    window.location.href = "../../pages/login.html"
+}
